196eb88368fa22fc92c35cfc15594d26447fe30b
Aleksandra Nitka <ola@trident-ww.com>
2021-06-05 21:30:34 +0200

    Add filter attr for api key auth

---
+++ CouponsLtd/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace CouponsLtd.Filters
+{
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method)]
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string ApiHeaderName = "ApiKey";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiHeaderName, out var potentialKey))
+                {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = configuration.GetValue<string>(ApiHeaderName);
+
+            if (apiKey != potentialKey)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
